94476e4dbe30940f10c7191b2ad6d24d1e22f3e0
s16701 <s16701@pjwstk.edu.pl>
2020-02-29 17:49:14 +0100

    dodane

---
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Cwiczenia1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var result = await client.GetAsync("http://www.pja.edu.pl");
+
+            if (result.IsSuccessStatusCode)
+            {
+                var html = await result.Content.ReadAsStringAsync();
+            }
+
+        }
+    }
+}
