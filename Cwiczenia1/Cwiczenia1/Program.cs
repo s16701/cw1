1cf75759e444b3d5861fd0db18105ee98925c5aa
s16701 <s16701@pjwstk.edu.pl>
2020-02-29 18:01:18 +0100

    dokonczone

--- Cwiczenia1/Cwiczenia1/Program.cs
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Cwiczenia1
@@ -7,13 +8,22 @@ namespace Cwiczenia1
     public class Program
     {
         public static async Task Main(string[] args)
+
         {
+            var url = args.Length > 0 ? args[0] : "http://www.pja.edu.pl";
             var client = new HttpClient();
-            var result = await client.GetAsync("http://www.pja.edu.pl");
+            var result = await client.GetAsync(url);
 
             if (result.IsSuccessStatusCode)
             {
                 var html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                var matches = regex.Matches(html);
+                foreach (var m in matches)
+                {
+                    Console.WriteLine(m.ToString());
+                }
             }
 
         }
